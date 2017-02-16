3f824403a89dde42d36f29ede833dcf6fd830f60
ombrecube <massj.info@gmail.com>
2017-02-16 13:33:36 -0500

    Swap Menu interface ok

--- app/src/main/java/prog/teampoule/applitest/HomePage.java
+++ app/src/main/java/prog/teampoule/applitest/HomePage.java
@@ -11,29 +11,8 @@ public class HomePage extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.menu);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch (id){
-            case R.id.id_patinoire:
-                Toast.makeText(getApplicationContext(),"Patinoire",Toast.LENGTH_SHORT);
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
