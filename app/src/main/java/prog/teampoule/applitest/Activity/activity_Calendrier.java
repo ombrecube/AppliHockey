5653c4774f3f73edcc601bfbc081ef49429671cc
CorentinGuimet <guimet.corentin@orange.fr>
2017-04-16 16:09:56 -0400

    Ajout calendrier

--- app/src/main/java/prog/teampoule/applitest/Activity/activity_Calendrier.java
+++ app/src/main/java/prog/teampoule/applitest/Activity/activity_Calendrier.java
@@ -1,8 +1,21 @@
 package prog.teampoule.applitest.Activity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.util.Date;
+import java.util.Locale;
 
 import prog.teampoule.applitest.Utilities.Menu;
 import prog.teampoule.applitest.R;
@@ -11,12 +24,55 @@ import prog.teampoule.applitest.R;
  * Created by Julien on 20/03/2017.
  */
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class activity_Calendrier extends Menu {
+
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ViewStub stub = (ViewStub) findViewById(R.id.layout_stub);
         stub.setLayoutResource(R.layout.activity_calendrier);
         View inflated = stub.inflate();
+
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        //Set an event for Teachers' Professional Day 2016 which is 21st of October
+
+        Event ev1 = new Event(Color.BLUE, 1477040400000L, "Teachers' Professional Day");
+
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+
+                if (dateClicked.toString().compareTo("Fri Oct 21 00:00:00 AST 2016") == 0) {
+                    Toast.makeText(context, "Teachers' Professional Day", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
+
+
+
     }
 }
