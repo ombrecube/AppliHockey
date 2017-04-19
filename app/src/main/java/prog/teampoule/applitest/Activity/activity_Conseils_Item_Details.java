53ee8ad62ac0275a1aed8b28530afa086e93ca40
ombrecube <massj.info@gmail.com>
2017-04-19 11:42:42 -0400

    Merging

---
+++ app/src/main/java/prog/teampoule/applitest/Activity/activity_Conseils_Item_Details.java
@@ -0,0 +1,251 @@
+package prog.teampoule.applitest.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import prog.teampoule.applitest.BDD.ConseilsBDD;
+import prog.teampoule.applitest.BDD.DetailsConseils;
+import prog.teampoule.applitest.BDD.MySQLiteHelper;
+import prog.teampoule.applitest.R;
+
+/**
+ * Created by Noemie on 22/03/2017.
+ */
+
+public class activity_Conseils_Item_Details extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conseils_details);
+
+        Intent intent = getIntent();
+        String item = intent.getExtras().getString("Item");
+
+        TextView viewTitre = (TextView) findViewById(R.id.titreDetails);
+        viewTitre.setText(item);
+
+        TextView viewTitreContenu1 = (TextView) findViewById(R.id.titreContenu1);
+        TextView viewTitreContenu2 = (TextView) findViewById(R.id.titreContenu2);
+        TextView viewTitreContenu3 = (TextView) findViewById(R.id.titreContenu3);
+        TextView viewTitreContenu4 = (TextView) findViewById(R.id.titreContenu4);
+        TextView viewTitreContenu5 = (TextView) findViewById(R.id.titreContenu5);
+        TextView viewTitreContenu6 = (TextView) findViewById(R.id.titreContenu6);
+        TextView viewTitreContenu7 = (TextView) findViewById(R.id.titreContenu7);
+        TextView viewTitreContenu8 = (TextView) findViewById(R.id.titreContenu8);
+        final TextView viewContenu1 = (TextView) findViewById(R.id.textContenu1);
+        final TextView viewContenu2 = (TextView) findViewById(R.id.textContenu2);
+        final TextView viewContenu3 = (TextView) findViewById(R.id.textContenu3);
+        final TextView viewContenu4 = (TextView) findViewById(R.id.textContenu4);
+        final TextView viewContenu5 = (TextView) findViewById(R.id.textContenu5);
+        final TextView viewContenu6 = (TextView) findViewById(R.id.textContenu6);
+        final TextView viewContenu7 = (TextView) findViewById(R.id.textContenu7);
+        final TextView viewContenu8 = (TextView) findViewById(R.id.textContenu8);
+
+        MySQLiteHelper db = new MySQLiteHelper(this);
+        ConseilsBDD conseils = new ConseilsBDD(db);
+
+        List<DetailsConseils> listDetails = new ArrayList<DetailsConseils>();
+        if (item.equals("Patins")) {
+            listDetails = conseils.getAllDetailsConseils(1);
+        } else if (item.equals("Bâtons")) {
+            listDetails = conseils.getAllDetailsConseils(2);
+        } else if (item.equals("Protections")) {
+            listDetails = conseils.getAllDetailsConseils(3);
+        }
+
+
+        String nom_details = new String();
+        String contenu_details = new String();
+
+        if (listDetails.size() > 0) {
+            nom_details = listDetails.get(0).getNom_details();
+            contenu_details = listDetails.get(0).getContenu_details();
+            viewTitreContenu1.setText(nom_details);
+            viewContenu1.setText(contenu_details);
+
+            viewTitreContenu1.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu1.getVisibility() == view.GONE) {
+                        viewContenu1.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu1.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu1.setText("");
+            viewContenu1.setText("");
+        }
+
+
+        if (listDetails.size() > 1) {
+            nom_details = listDetails.get(1).getNom_details();
+            contenu_details = listDetails.get(1).getContenu_details();
+            viewTitreContenu2.setText(nom_details);
+            viewContenu2.setText(contenu_details);
+
+            viewTitreContenu2.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu2.getVisibility() == view.GONE) {
+                        viewContenu2.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu2.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu2.setText("");
+            viewContenu2.setText("");
+        }
+
+        if (listDetails.size() > 2) {
+            nom_details = listDetails.get(2).getNom_details();
+            contenu_details = listDetails.get(2).getContenu_details();
+            viewTitreContenu3.setText(nom_details);
+            viewContenu3.setText(contenu_details);
+
+            viewTitreContenu3.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu3.getVisibility() == view.GONE) {
+                        viewContenu3.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu3.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu3.setText("");
+            viewContenu3.setText("");
+        }
+
+        if (listDetails.size() > 3) {
+            nom_details = listDetails.get(3).getNom_details();
+            contenu_details = listDetails.get(3).getContenu_details();
+            viewTitreContenu4.setText(nom_details);
+            viewContenu4.setText(contenu_details);
+
+            viewTitreContenu4.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu4.getVisibility() == view.GONE) {
+                        viewContenu4.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu4.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu4.setText("");
+            viewContenu4.setText("");
+        }
+
+        if (listDetails.size() > 4) {
+            nom_details = listDetails.get(4).getNom_details();
+            contenu_details = listDetails.get(4).getContenu_details();
+            viewTitreContenu5.setText(nom_details);
+            viewContenu5.setText(contenu_details);
+
+            viewTitreContenu5.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu5.getVisibility() == view.GONE) {
+                        viewContenu5.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu5.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu5.setText("");
+            viewContenu5.setText("");
+        }
+
+        if (listDetails.size() > 5) {
+            nom_details = listDetails.get(5).getNom_details();
+            contenu_details = listDetails.get(5).getContenu_details();
+            viewTitreContenu6.setText(nom_details);
+            viewContenu6.setText(contenu_details);
+
+            viewTitreContenu6.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu6.getVisibility() == view.GONE) {
+                        viewContenu6.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu6.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu6.setText("");
+            viewContenu6.setText("");
+        }
+
+        if (listDetails.size() > 6) {
+            nom_details = listDetails.get(6).getNom_details();
+            contenu_details = listDetails.get(6).getContenu_details();
+            viewTitreContenu7.setText(nom_details);
+            viewContenu7.setText(contenu_details);
+
+<<<<<<< HEAD
+            viewContenu7.setOnClickListener(new OnClickListener() {
+=======
+            viewTitreContenu7.setOnClickListener(new OnClickListener() {
+>>>>>>> 500f2001f42b1fbfa756d2e0f4366fb85005387e
+                public void onClick(View view) {
+                    if (viewContenu7.getVisibility() == view.GONE) {
+                        viewContenu7.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu7.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu7.setText("");
+            viewContenu7.setText("");
+        }
+
+        if (listDetails.size() > 7) {
+            nom_details = listDetails.get(7).getNom_details();
+            contenu_details = listDetails.get(7).getContenu_details();
+            viewTitreContenu8.setText(nom_details);
+            viewContenu8.setText(contenu_details);
+
+            viewTitreContenu8.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) {
+                    if (viewContenu8.getVisibility() == view.GONE) {
+                        viewContenu8.setVisibility(view.VISIBLE);
+                    } else {
+                        viewContenu8.setVisibility(view.GONE);
+                    }
+                }
+            });
+
+        } else {
+            viewTitreContenu8.setText("");
+            viewContenu8.setText("");
+        }
+
+
+        //Toast.makeText(getApplicationContext(), "problème", Toast.LENGTH_LONG).show();
+        //Log.d("salut", item.getTitre().toString());
+    }
+}
---
+++ app/src/main/java/prog/teampoule/applitest/Activity/activity_friend.java
@@ -0,0 +1,168 @@
+package prog.teampoule.applitest.Activity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.TabLayout.OnTabSelectedListener;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import prog.teampoule.applitest.R;
+import prog.teampoule.applitest.Utilities.HttpRequestTask_Friend;
+import prog.teampoule.applitest.Utilities.Menu;
+import prog.teampoule.applitest.classAdapter.AdapteurUser;
+import prog.teampoule.applitest.classAdapter.User;
+
+/**
+ * Created by Julien on 09/04/2017.
+ */
+
+public class activity_friend extends Menu{
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    public static ListView lv ;
+    public static ListView lv2;
+    public static AdapteurUser adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menutabbed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        if(is_Connected){
+            navigationView.getMenu().findItem(R.id.Menu_itemAmis).setVisible(true);
+        }else{
+            navigationView.getMenu().findItem(R.id.Menu_itemAmis).setVisible(false);
+        }
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        lv = new ListView(getApplicationContext());
+        lv2 = new ListView(getApplicationContext());
+        adapter = new AdapteurUser(getApplicationContext(),new ArrayList<User>());
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        GetFriend();
+    }
+
+    public void GetFriend() {
+        SharedPreferences prefs = getSharedPreferences("MYPREF", MODE_PRIVATE);
+        int id = prefs.getInt("id_user",-1);
+        String myurl = "https://patinou-rest-api-ombrecube.c9users.io/amis/"+ String.valueOf(id);
+        HttpRequestTask_Friend http = new HttpRequestTask_Friend();
+        http.setURL(myurl);
+        http.setContext(getApplicationContext());
+        http.setFLAG(1);
+        http.setListView(lv);
+        http.setAdapteur(adapter);
+        http.execute();
+    }
+
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0){
+                return ListHolderFragment.newInstance(position);
+            }else if (position == 1){
+                return ListHolderFragment.newInstance(position);
+            }else{
+                return ListHolderFragment.newInstance(position);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Mes amis";
+                case 1:
+                    return "Demande amis";
+                case 2:
+                    return "Chercher des amis";
+            }
+            return null;
+        }
+    }
+    public static class ListHolderFragment extends Fragment {
+        private static String ARG_SECTION_NUMBER;
+
+        public  ListHolderFragment(){}
+
+        public static ListHolderFragment newInstance(int sectionNumber) {
+            ListHolderFragment fragment = new ListHolderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView;
+            Log.d("SectionNumner",String.valueOf(ARG_SECTION_NUMBER));
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 0) {
+                rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                lv = (ListView) rootView.findViewById(R.id.lv_Amis);
+                TextView textView = (TextView) rootView.findViewById(R.id.textView7);
+                textView.setText("Mes Amis");
+            }else if(getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+                rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                lv2 = (ListView) rootView.findViewById(R.id.lv_Amis);
+                TextView textView = (TextView) rootView.findViewById(R.id.textView7);
+                textView.setText("Demander amis");
+            }else{
+                rootView = inflater.inflate(R.layout.fragmentlist , container, false);
+                lv2 = (ListView) rootView.findViewById(R.id.lv_Requested);
+            }
+
+          return rootView;
+        }
+    }
+}
